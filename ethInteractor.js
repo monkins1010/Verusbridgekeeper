853e363a0f1f7a47fa0368af7900fb7681a87755
Asher Dawes <asher.dawer@gmail.com>
2024-09-08 20:23:15 -0700

    Fix ticker check

--- ethInteractor.js
+++ ethInteractor.js
@@ -1081,7 +1081,7 @@ async function checkProofRoot(height, stateroot, blockhash, power) {
         let gasPriceInSATS = (BigInt(transaction.gasPrice) / BigInt(10))
 
         latestproofroot.height = block.number;
-        if (latestproofroot.height < (config.ticker === "VRSCTEST" ? constants.TESTNET_ETH_GAS_REDUCTION_HEIGHT : constants.ETH_GAS_REDUCTION_HEIGHT))
+        if (latestproofroot.height < (InteractorConfig.ticker === "VRSCTEST" ? constants.TESTNET_ETH_GAS_REDUCTION_HEIGHT : constants.ETH_GAS_REDUCTION_HEIGHT))
         {
             latestproofroot.gasprice = gasPriceInSATS < BigInt(1000000000) ? "10.00000000" : util.uint64ToVerusFloat(gasPriceInSATS);
         }
